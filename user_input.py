8ff73a85a7bf2b773df5e394841d2e22f7a36340
Bhekumuzi Tshabalala <Bhekumuzitshabalala.main@gmail.com>
2024-02-21 15:43:20 +0200

    done

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = input("How old are you?")
+location = input("Where do you live?")
+
+print("Your name is " + name + " you are  " + age + " years old . You live in " + location )
